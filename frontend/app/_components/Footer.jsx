ce9cf512525e218d6215fe096f5ebe59e04a2c3e
Full Stack Developer <161573297+spring-boot-react@users.noreply.github.com>
2024-03-18 11:56:56 +0100

    Implement Frontend Infrastructure and Layout with Next.js

---
+++ frontend/app/_components/Footer.jsx
@@ -0,0 +1,37 @@
+export default function Footer() {
+  return (
+    <>
+      <div className="bottom">
+        
+        {/* <!-- Contact section --> */}
+        <div className="flex bg-teal-950">
+          <div className="w-1/4">
+            <p>Text 1</p>
+            <p>Text 2</p>
+            <p>Text 3</p>
+          </div>
+          <div className="w-1/4">
+            <p>Text 4</p>
+            <p>Text 5</p>
+            <p>Text 6</p>
+          </div>
+          <div className="w-1/4">
+            <p>Text 7</p>
+            <p>Text 8</p>
+            <p>Text 9</p>
+          </div>
+          <div className="w-1/4">
+            <p>Text 10</p>
+            <p>Text 11</p>
+            <p>Text 12</p>
+          </div>
+        </div>
+
+        {/* <!--Copyright section--> */}
+        <div className="w-1/1 bg-teal-950">
+          <p>This should be one line</p>
+        </div>
+      </div>
+    </>
+  );
+}
